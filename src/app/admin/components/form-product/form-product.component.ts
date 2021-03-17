684c2badea5c248223eb37cb089954a18bca393e
John Hernández <johnenderhernandez@gmail.com>
2021-03-17 16:15:09 -0600

    adding custom validation

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MyValidators} from './../../../utils/validators'
+
+import { ProductsService } from './../../../core/services/products/products.service'
 
 @Component({
   selector: 'app-form-product',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormProductComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
+  ) { 
+    this.buildForm();
+  }
 
   ngOnInit(): void {
   }
 
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      id: ['', [Validators.required, Validators.maxLength(1000)]],
+      title: ['', [Validators.required, Validators.maxLength(1000)]],
+      price: [0, [Validators.required, Validators.maxLength(1000), MyValidators.isPriceValid]],
+      image: ['', [Validators.maxLength(1000)]],
+      description: ['', [Validators.required, Validators.maxLength(1000)]],
+    })
+  }
+
+  createProduct(event:Event){
+    event.preventDefault();
+    if (this.form.valid){
+      const product = this.form.value;
+      this.productsService.createProduct(product).subscribe((newProduct) => {
+        console.log(newProduct);
+        this.router.navigate(['./admin/products'])
+      })
+      
+    }
+  }
 }
