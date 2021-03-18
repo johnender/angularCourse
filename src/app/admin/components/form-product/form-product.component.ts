e52f8e569539af106d1f63ab50f5a292d907d07a
John Hernández <johnenderhernandez@gmail.com>
2021-03-17 20:42:35 -0600

    adding upload to firebase storage

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -6,6 +6,11 @@ import { MyValidators} from './../../../utils/validators'
 
 import { ProductsService } from './../../../core/services/products/products.service'
 
+import { AngularFireStorage } from '@angular/fire/storage'
+
+import { finalize } from 'rxjs/operators'
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-form-product',
   templateUrl: './form-product.component.html',
@@ -14,10 +19,14 @@ import { ProductsService } from './../../../core/services/products/products.serv
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
+
+
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage,
   ) { 
     this.buildForm();
   }
@@ -45,5 +54,24 @@ export class FormProductComponent implements OnInit {
       })
       
     }
+  }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const name = event.target.files[0].name;
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+    .pipe(
+      finalize(() => {
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe(url => {
+          console.log(url);
+          this.form.get('image').setValue(url);
+        });
+      })
+    )
+    .subscribe();
   }
 }
